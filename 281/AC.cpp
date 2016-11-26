8b0651f32a55525017617ae32bb5447ef2671e76
xiplus <huangxuanyuxiplus@gmail.com>
2016-11-26 15:18:30 +0800

    281 revise testdata

--- 281/AC.cpp
+++ 281/AC.cpp
@@ -10,6 +10,16 @@ int main(){
     cin>>t;
     while(t--){
         cin>>x1>>y1>>x2>>y2>>x3>>y3;
-        cout<<(max({x1,x2,x3})-min({x1,x2,x3})+1)*(max({y1,y2,y3})-min({y1,y2,y3})+1)-((abs(x1-x2)+1)*(abs(y1-y2)+1)-__gcd(abs(x1-x2),abs(y1-y2))-1)/2-((abs(x1-x3)+1)*(abs(y1-y3)+1)-__gcd(abs(x1-x3),abs(y1-y3))-1)/2-((abs(x2-x3)+1)*(abs(y2-y3)+1)-__gcd(abs(x2-x3),abs(y2-y3))-1)/2<<endl;
+        int ans=(max({x1,x2,x3})-min({x1,x2,x3})+1)*(max({y1,y2,y3})-min({y1,y2,y3})+1)
+		-((abs(x1-x2)+1)*(abs(y1-y2)+1)-__gcd(abs(x1-x2),abs(y1-y2))-1)/2
+		-((abs(x1-x3)+1)*(abs(y1-y3)+1)-__gcd(abs(x1-x3),abs(y1-y3))-1)/2
+		-((abs(x2-x3)+1)*(abs(y2-y3)+1)-__gcd(abs(x2-x3),abs(y2-y3))-1)/2;
+		if(x1!=max({x1,x2,x3})&&x1!=min({x1,x2,x3})&&y1!=max({y1,y2,y3})&&y1!=min({y1,y2,y3}))
+			ans-=min(abs(x1-x2)*abs(y1-y3),abs(x1-x3)*abs(y1-y2));
+		if(x2!=max({x1,x2,x3})&&x2!=min({x1,x2,x3})&&y2!=max({y1,y2,y3})&&y2!=min({y1,y2,y3}))
+			ans-=min(abs(x2-x1)*abs(y2-y3),abs(x2-x3)*abs(y2-y1));
+		if(x3!=max({x1,x2,x3})&&x3!=min({x1,x2,x3})&&y3!=max({y1,y2,y3})&&y3!=min({y1,y2,y3}))
+			ans-=min(abs(x3-x1)*abs(y3-y2),abs(x3-x2)*abs(y3-y1));
+        cout<<ans<<endl;
     }
 }
--- 281/data.php
+++ 281/data.php
@@ -1,35 +1,49 @@
 <?php
-$type=3;
+file_put_contents("res/testdata/1.in", "1
+3 7 -2 -2 7 -1
+");
+function data1($n) {
+	$x1=$y1=$x2=$y2=0;
+	while ($x1==$x2||$y1==$y2) {
+		$x1=rand(-$n,$n);
+		$y1=rand(-$n,$n);
+		$x2=rand(-$n,$n);
+		$y2=rand(-$n,$n);
+	}
+	$temp=rand(0,1);
+	if($temp)return $x1." ".$y1." ".$x2." ".$y2." ".$x1." ".$y2."\n";
+	else return $x1." ".$y1." ".$x2." ".$y2." ".$x2." ".$y1."\n";
+}
+function data2($n) {
+	$x1=$y1=$x2=$y2=$x3=$y3=0;
+	while ($x1==$x2||$x2==$x3||$y1==$y2||$y2==$y3){
+		$x1=rand(-$n,$n);
+		$y1=rand(-$n,$n);
+		$x2=rand(-$n,$n);
+		$y2=rand(-$n,$n);
+		$x3=rand(-$n,$n);
+		$y3=rand(-$n,$n);
+	}
+	return $x1." ".$y1." ".$x2." ".$y2." ".$x3." ".$y3."\n";
+}
 $data="10000\n";
-if($type==2){
-	for($i=0;$i<10000;$i++){
-		$x1=rand(-1000,1000);
-		$y1=rand(-1000,1000);
-		$x2=rand(-1000,1000);
-		$y2=rand(-1000,1000);
-		if($x1==$x2||$y1==$y2){
-			$i--;
-			continue;
-		}
-		$temp=rand(0,1);
-		if($temp)$data.=$x1." ".$y1." ".$x2." ".$y2." ".$x1." ".$y2."\n";
-		else $data.=$x1." ".$y1." ".$x2." ".$y2." ".$x2." ".$y1."\n";
-	}
+for($i=0;$i<10000;$i++){
+	$data.=data1(10);
 }
-else if($type==3){
-	for($i=0;$i<10000;$i++){
-		$x1=rand(-1000,1000);
-		$y1=rand(-1000,1000);
-		$x2=rand(-1000,1000);
-		$y2=rand(-1000,1000);
-		$x3=rand(-1000,1000);
-		$y3=rand(-1000,1000);
-		if($x1==$x2||$x2==$x3||$y1==$y2||$y2==$y3){
-			$i--;
-			continue;
-		}
-		$data.=$x1." ".$y1." ".$x2." ".$y2." ".$x3." ".$y3."\n";
-	}
+file_put_contents("res/testdata/2.in", $data);
+$data="10000\n";
+for($i=0;$i<10000;$i++){
+	$data.=data1(1000);
 }
-file_put_contents("0.in",$data);
+file_put_contents("res/testdata/3`.in", $data);
+$data="10000\n";
+for($i=0;$i<10000;$i++){
+	$data.=data2(10);
+}
+file_put_contents("res/testdata/4.in", $data);
+$data="10000\n";
+for($i=0;$i<10000;$i++){
+	$data.=data2(1000);
+}
+file_put_contents("res/testdata/5.in", $data);
 ?>
