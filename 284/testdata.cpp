1b2fa102a63eb7ed60a1aa71ff6eb1828981abe2
xiplus <huangxuanyuxiplus@gmail.com>
2017-02-05 12:01:06 +0800

    update 284 testdata

--- 284/testdata.cpp
+++ 284/testdata.cpp
@@ -5,30 +5,37 @@ using namespace std;
 int main(){
 //	ios::sync_with_stdio(false);
 //	cin.tie(0);
-	srand(time(NULL));
+	srand(7122);
 	int n,m;
-	for(int q=0;q<1000;q++){
-		n=rand()%20+1;
-		switch(n){
-			case 1 ... 10:
-				m=rand()%10000+1;
-				cout<<1<<" "<<m<<endl;
-				break;
-			case 11 ... 15:
-				m=rand()%10000+1;
-				cout<<2<<" "<<m<<endl;
-				break;
-			case 16 ... 17:
-				cout<<3<<endl;
-				break;
-			case 18 ... 19:
-				cout<<4<<endl;
-				break;
-			case 20:
-				m=rand()%5000+1;
-				cout<<5<<" "<<m<<endl;
-				break;
+	for(int q=1;q<=100;q++){
+		fstream file;
+		stringstream s;
+		s<<q<<".in";
+		file.open(s.str().c_str(), ios::out);
+		for(int w=0;w<q*10;w++){
+			n=rand()%20+1;
+			switch(n){
+				case 1 ... 10:
+					m=rand()%10000+1;
+					file<<1<<" "<<m<<endl;
+					break;
+				case 11 ... 15:
+					m=rand()%10000+1;
+					file<<2<<" "<<m<<endl;
+					break;
+				case 16 ... 17:
+					file<<3<<endl;
+					break;
+				case 18 ... 19:
+					file<<4<<endl;
+					break;
+				case 20:
+					m=rand()%5000+1;
+					file<<5<<" "<<m<<endl;
+					break;
+			}
 		}
+		file<<0<<endl;
+		file.close();
 	}
-	cout<<0<<endl;
 }
