5fc8e245fc848780eb12dc2306cc8bfe8461cd46
xiplus <huangxuanyuxiplus@gmail.com>
2021-09-10 22:19:52 +0800

    Add 631 checker

---
+++ 631/res/check/check.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+
+with os.fdopen(2) as f:
+    Answer = float(f.readline())
+
+with open(sys.argv[3]) as f:
+    try:
+        Output = float(sys.stdin.readline())
+    except:
+        exit(-1)
+
+try:
+    Ae = abs(Answer-Output)
+    Re = Ae/Answer
+
+    if Ae <= 0.000001 or Re <= 0.000001:
+        exit(0)
+    else:
+        exit(-1)
+except:
+	exit(-1)
